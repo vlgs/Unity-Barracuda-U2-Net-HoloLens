279a39f0d54c8a20b6c2636ebd27b208ccbf0cfa
Valentin LOGEAIS <valentin.logeais@immersion.fr>
2021-03-04 23:34:49 +0100

    Working on HoloLens 2

--- Assets/Scripts/Inference.cs
+++ Assets/Scripts/Inference.cs
@@ -1,10 +1,15 @@
-﻿#define WEBCAM
+﻿#define WEBCAM 
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Barracuda;
+using System.Linq;
+using System;
 
+#if WEBCAM && UNITY_WSA //&& !UNITY_EDITOR
+using UnityEngine.Windows.WebCam;
+#endif
 
 public class Inference : MonoBehaviour
 {
@@ -24,7 +29,11 @@ public class Inference : MonoBehaviour
 
     public int inputResolutionY = 32;
     public int inputResolutionX = 32;
-    
+
+#if UNITY_WSA
+    UnityEngine.Windows.WebCam.VideoCapture m_VideoCapture = null;
+#endif
+
     void Start()
     {
         Application.targetFrameRate = 60;
@@ -32,17 +41,55 @@ public class Inference : MonoBehaviour
         m_RuntimeModel = ModelLoader.Load(inputModel, false);
         m_Worker = WorkerFactory.CreateWorker(WorkerFactory.Type.ComputePrecompiled, m_RuntimeModel, false);
 
-
 #if (WEBCAM)
+
+#if UNITY_WSA 
+        Resolution cameraResolution = VideoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
+        Debug.Log(cameraResolution);
+
+        float cameraFramerate = VideoCapture.GetSupportedFrameRatesForResolution(cameraResolution).OrderByDescending((fps) => fps).First();
+        Debug.Log(cameraFramerate);
+
+        VideoCapture.CreateAsync(false, delegate (VideoCapture videoCapture)
+        {
+            if (videoCapture != null)
+            {
+                m_VideoCapture = videoCapture;
+                //Debug.Log("Created VideoCapture Instance!");
+
+                CameraParameters cameraParameters = new CameraParameters();
+                cameraParameters.hologramOpacity = 0.0f;
+                cameraParameters.frameRate = cameraFramerate;
+                cameraParameters.cameraResolutionWidth = cameraResolution.width;
+                cameraParameters.cameraResolutionHeight = cameraResolution.height;
+                cameraParameters.pixelFormat = CapturePixelFormat.BGRA32;
+
+                m_VideoCapture.StartVideoModeAsync(cameraParameters,
+                                                   VideoCapture.AudioState.ApplicationAndMicAudio,
+                                                   OnStartedVideoCaptureMode);
+            }
+            else
+            {
+                Debug.LogError("Failed to create VideoCapture Instance!");
+            }
+        });
+#else
         m_WebcamTexture = new WebCamTexture();
         m_WebcamTexture.Play();
+#endif
+
 #else
         var targetRT = RenderTexture.GetTemporary(inputResolutionX, inputResolutionY, 0);
         Graphics.Blit(inputImage, targetRT, postprocessMaterial);
         m_Input = new Tensor(targetRT, 3);
 
-        m_Input = new Tensor(1, inputResolutionY, inputResolutionX, 3);
+        //m_Input = new Tensor(1, inputResolutionY, inputResolutionX, 3);
 #endif
+    }
+
+    private void OnStartedVideoCaptureMode(VideoCapture.VideoCaptureResult result)
+    {
+        throw new NotImplementedException();
     }
 
     void Update()
@@ -52,6 +99,8 @@ public class Inference : MonoBehaviour
         Graphics.Blit(m_WebcamTexture, targetRT, postprocessMaterial);
 
         Tensor input = new Tensor(targetRT, 3);
+#else
+        Tensor input = m_Input;
 #endif
         m_Worker.Execute(input);
         Tensor result = m_Worker.PeekOutput("output");
